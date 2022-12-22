9cf5cfda5c8969ea62fc92f9e8940d6fd8340875
Raissa Barbosa <raissabbsa@gmail.com>
2022-12-21 21:26:20 -0300

    feat: pratica 6

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Praticas-Postgres
+
+pratica 6 - db designer: https://app.dbdesigner.net/designer/schema/588718
--- pratica6-agencia-de-viagens/exercicio.sql
+++ pratica6-agencia-de-viagens/exercicio.sql
@@ -1,1 +1,23 @@
-CREATE DATABASE 
+CREATE DATABASE "travelAgencies";
+
+CREATE TABLE airport(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    surname TEXT NOT NULL
+);
+
+CREATE TABLE companies(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    surname TEXT NOT NULL
+);
+
+CREATE TABLE flights(
+    id SERIAL NOT NULL PRIMARY KEY,
+    surname TEXT NOT NULL,
+    "companyId" INTEGER NOT NULL REFERENCES companies(id),
+    "airportOriginId" INTEGER NOT NULL REFERENCES airport(id),
+    "airportDestinyId" INTEGER NOT NULL REFERENCES airport(id),
+    "exitTime" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "arrivalTime" TIMESTAMP   
+);
